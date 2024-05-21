b2810d2afd25623d14b71c450f96f667cf735d5f
Hlib Haranin <71976818+GLEF1X@users.noreply.github.com>
2024-05-21 19:55:04 -0400

    Time: 41 ms (36.98%) | Memory: 16.7 MB (8.80%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -0,0 +1,51 @@
+MAX_ALLOWED_INTEGER = 2 ** 31 - 1
+MIN_ALLOWED_INTEGER = -2 ** 31
+
+
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        s = s.lstrip()
+        if s == "":
+            return 0
+
+        sign = 1
+        possible_sign = s[0]
+        if possible_sign == "+":
+            s = s[1:]
+        elif possible_sign == "-":
+            sign = -1
+            s = s[1:]
+        
+        if s == "":
+            return 0
+
+        is_checking_first_digits = True
+        skipped_indexes = 0
+        for i, char in enumerate(s):
+            if not char.isdigit():
+                s = s[:i - skipped_indexes]
+                if s == "":
+                    return 0
+
+                return self.keep_integer_in_bounds_of_32_bits(int(s) * sign)
+
+            if is_checking_first_digits and char == "0":
+                s = s[1:]
+                skipped_indexes += 1
+                continue
+
+            is_checking_first_digits = False
+
+        if s == "":
+            return 0
+
+        return self.keep_integer_in_bounds_of_32_bits(int(s) * sign)
+
+    @staticmethod
+    def keep_integer_in_bounds_of_32_bits(i: int) -> int:
+        if i < MIN_ALLOWED_INTEGER:
+            return MIN_ALLOWED_INTEGER
+        elif i > MAX_ALLOWED_INTEGER:
+            return MAX_ALLOWED_INTEGER
+
+        return i
