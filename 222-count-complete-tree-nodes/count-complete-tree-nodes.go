48a89523264efab5cba32134f4c6fe7ef9f8972f
Hlib Haranin <71976818+GLEF1X@users.noreply.github.com>
2024-05-24 11:52:36 -0400

    Time: 18 ms (21.58%) | Memory: 7.5 MB (43.34%) - LeetSync

---
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.go
@@ -0,0 +1,65 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func countNodes(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	leftDepth := countLeftDepth(root)
+	rightDepth := countRightDepth(root)
+
+	if leftDepth == rightDepth {
+		return IntPow(2, leftDepth) - 1
+	}
+
+	return countNodes(root.Left) + countNodes(root.Right) + 1
+}
+
+func countLeftDepth(node *TreeNode) int {
+	if node.Left == nil {
+		return 1
+	}
+
+	return 1 + countLeftDepth(node.Left)
+}
+
+func countLeftDepthIterative(node *TreeNode) int {
+	var count int
+	currentNode := node
+	for currentNode != nil {
+		count++
+		currentNode = currentNode.Left
+	}
+
+	return count
+}
+
+func countRightDepth(node *TreeNode) int {
+	if node.Right == nil {
+		return 1
+	}
+
+	return 1 + countRightDepth(node.Right)
+}
+
+func IntPow(base, exp int) int {
+	result := 1
+	for {
+		if exp&1 == 1 {
+			result *= base
+		}
+		exp >>= 1
+		if exp == 0 {
+			break
+		}
+		base *= base
+	}
+
+	return result
+}
