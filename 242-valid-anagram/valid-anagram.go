7b8b6326474c3fea019ddc8cf9919a13c5f66ec0
Hlib Haranin <71976818+GLEF1X@users.noreply.github.com>
2024-06-14 03:33:35 -0400

    Time: 3 ms (78.73%) | Memory: 3 MB (99.15%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.go
@@ -0,0 +1,19 @@
+func isAnagram(s string, t string) bool {
+    chars := make(map[rune]int)
+    
+    for _, v := range s {
+        chars[v]++
+    }
+    
+    for _, v := range t {
+        chars[v]--
+    }
+    
+    for _, v := range chars {
+        if v != 0 {
+            return false
+        }
+    }
+    
+    return true
+}
