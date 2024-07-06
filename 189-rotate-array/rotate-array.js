4912fc0f2973c5aabe172cb1cbc426ca2194ec86
Gleb Haranin <71976818+GLEF1X@users.noreply.github.com>
2024-07-06 12:42:23 -0400

    Time: 85 ms (42.80%) | Memory: 60.1 MB (27.01%) - LeetSync

---
+++ 189-rotate-array/rotate-array.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    k = k % nums.length; // Handle cases where k > nums.length
+    if (k === 0) return; // No rotation needed
+
+    reverse(nums, 0, nums.length - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, nums.length - 1);
+};
+
+function reverse(nums, start, end) {
+    while (start < end) {
+        let temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        start++;
+        end--;
+    }
+}
