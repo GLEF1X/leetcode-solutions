16a0cedaf474cc1df7fc93217ac8aff57d154752
Gleb Haranin <71976818+GLEF1X@users.noreply.github.com>
2024-07-27 23:24:19 -0400

    Time: 1 ms (69.57%) | Memory: 2.7 MB (73.13%) - LeetSync

---
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.go
@@ -0,0 +1,20 @@
+import "strings"
+
+// 6 / 2 = 3
+// 4 / 2 = 2
+func gcdOfStrings(str1 string, str2 string) string {
+    if str1 + str2 != str2 + str1 {
+        return ""
+    }
+
+    return str1[:GCD(len(str1), len(str2))]
+}
+
+func GCD(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
